2f73703f48c0ce6da07d09d1d9df0c89ec2d7b2c
Kelly Martinez <kelly.martinez@boomtechny.com>
2021-04-15 10:41:45 -0400

    lambda support

--- src/server/index.tsx
+++ src/server/index.tsx
@@ -12,6 +12,7 @@ import { Context, Package } from "@frontity/types";
 import { getSnapshot } from "@frontity/connect";
 import { ChunkExtractor } from "@loadable/server";
 import getTemplate from "./templates";
+import awsServerlessExpress from "aws-serverless-express";
 import {
   getStats,
   hasEntryPoint,
@@ -24,6 +25,7 @@ import { FrontityTags } from "../../types";
 import createStore from "./store";
 import { exists } from "fs";
 import { promisify } from "util";
+import serverless from 
 
 /**
  * Options for {@link server}.
@@ -199,6 +201,32 @@ const server = ({ packages }: ServerOptions): ReturnType<Koa["callback"]> => {
     ctx.body = template({ html, frontity, head });
     next();
   });
+// Make sure the porxy headers are forwarded
+app.proxy = true;
+
+// This middleware is from this discussion: https://community.frontity.org/t/deploy-to-aws-lambda/814/19?u=cristian.bote
+app.use((ctx, next) => {
+  ctx.lambdaEvent =
+    (ctx.headers["x-apigateway-event"] &&
+      JSON.parse(decodeURIComponent(ctx.headers["x-apigateway-event"]))) ||
+    {};
+  ctx.lambdaContext =
+    (ctx.headers["x-apigateway-context"] &&
+      JSON.parse(decodeURIComponent(ctx.headers["x-apigateway-context"]))) ||
+    {};
+  ctx.env = (ctx.lambdaEvent && ctx.lambdaEvent.stageVariables) || process.env;
+
+  // Workaround an inconsistency in APIG. For custom domains, it puts the
+  // mapping prefix on the url, but non-custom domain requests do not. Fix it by
+  // changing the path to the proxy param which has the correct value always.
+  if (ctx.lambdaEvent.pathParameters && ctx.lambdaEvent.pathParameters.proxy) {
+    const dummyBase = "zz://zz";
+    const url = new URL(ctx.url, dummyBase);
+    url.pathname = "/" + ctx.lambdaEvent.pathParameters.proxy;
+    ctx.url = url.href.replace(dummyBase, "");
+  }
+  return next();
+});
 
   return app.callback();
 };
