e73ccd4e33fc3db9f2201fb813c41465ea8facf0
Kelly Martinez <kelly.martinez@boomtechny.com>
2021-07-22 01:31:25 -0400

    Redirection Support

--- src/server/utils/redirection/index.js
+++ src/server/utils/redirection/index.js
@@ -1,47 +1,63 @@
 
 const redirection = (ctx) =>{
 
-    if(ctx['response'].status ==404){
-        console.log('error found');
-        var originalURL = ctx['url'];
-        console.log('check original url', originalURL);
-        let baseurl = `${process.env.LOCALSITE}/js/redirects.json`;
-        const request_data = {
-          url: baseurl,
-          method: "GET",
-        };
-      
-        fetch(request_data.url,{
-          method: request_data.method}
-        ).then((resp) => resp.json()).then((response) => {
+  if(ctx['response'].status ==404){
+      //console.log('error found');
+      var originalURL = ctx['url'];
+      console.log('check redirection original url', originalURL);
+      let baseurl = `${process.env.LOCALSITE}/js/redirects.json`;
+      const request_data = {
+        url: baseurl,
+        method: "GET",
+      };
+    var firstMatch =false;
+      fetch(request_data.url,{
+        method: request_data.method}
+      ).then((resp) => resp.json()).then((response) => {
 
-            var redirects =response.redirects;
-            var count = redirects.length; 
-           
-            var redirectURL = "";
-           let compareLink = originalURL.toLowerCase();
-          // console.log('check compare link', compareLink);
-            for(let i=0; i<redirects.length;i++){
-              //console.log(compareLink.startsWith(redirects[i].match_url));
-             if(compareLink.includes(redirects[i].match_url)==true){
-               
-          
+          var redirects =response.redirects;
+          var count = redirects.length; 
+         
+          var redirectURL = "";
+
+         let compareLink = originalURL.replace(/\/$/, '').toLowerCase();
+         //console.log('check compare link', compareLink);
+          for(let i=0; i<redirects.length;i++){
+            //console.log(compareLink.startsWith(redirects[i].match_url));
+            let redirectListURL = redirects[i].url.replace(/\/$/, '').toLowerCase();
+            //console.log('check redirectList url', redirectListURL);
+           if(redirectListURL === compareLink){
              
-               //if(redirects[i].match_url== compareLink){
-                 redirectURL = redirects[i].action_data.url;
-                 ctx.redirect(redirectURL);
-              // }
-        
-               }
-            }
-
-        })
-        .catch(() => false );
-        }
       
-      else
-      return;
-    };
-    export default redirection;
+              if(firstMatch==false){
+
+             
+             //if(redirects[i].match_url== compareLink){
+           
+               redirectURL = redirects[i].action_data.url;
+               
+               if (redirectURL.indexOf('://') > 0 || redirectURL.indexOf('//') === 0 ) {
+                   firstMatch=true;
+                  ctx.redirect(redirectURL);
+              } else {
+              firstMatch=true;
+               // console.log('check final redirect', `http://localhost:3000`+redirectURL);
+                ctx.redirect(`${process.env.LOCALSITE}`+redirectURL);
+                
+              }
+          }
+            // }
+      
+             }
+          }
+
+      })
+      .catch(() => false );
+      }
     
-    
+    else
+    return;
+  };
+  export default redirection;
+  
+  
